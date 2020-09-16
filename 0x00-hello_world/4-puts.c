61ccbc9a50807e72dcc2bb111195ab8b320452bf
azizzouaghi <2181@holbertonschool.com>
2020-09-16 20:30:40 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-int main( int argc, char * argv[] ) {
-
-    /* do you not steal */
-    puts( "Programming is like building a multilingual puzzle" );    
-        
-        
-    return 0;
+/**
+ * main
+ * "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
