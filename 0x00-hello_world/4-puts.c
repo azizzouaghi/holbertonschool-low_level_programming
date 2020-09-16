1fe59255824148c46b3ed3ae16d610a62f502a4a
azizzouaghi <2181@holbertonschool.com>
2020-09-16 20:41:31 +0100

    putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main
- * "\"Programming is like building a multilingual puzzle"
- * Return: 0
- */
+* main
+* "\"Programming is like building a multilingual puzzle"
+* Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
