d184a95d1dac7a763e1d877ea4a769438d960698
azizzouaghi <2181@holbertonschool.com>
2020-09-16 17:17:31 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main( int argc, char * argv[] ) {
+
+    /* do you not steal */
+    puts( "Programming is like building a multilingual puzzle" );    
+        
+        
+    return 0;
+}
