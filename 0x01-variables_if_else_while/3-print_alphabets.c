ac4b601405e0bcec120c1ce670f7c2c82f87fc50
azizzouaghi <2181@holbertonschool.com>
2020-09-17 23:23:17 +0100

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,14 @@
 * Return: Always 0
 */
 int main(void)
-{
+
 int  letter;
-for (letter = 'A'; letter <= 'Z'; letter++)
 {
-putchar (letter);
-}
-putchar('\n');
+char letter;  
+for (letter = 'a'; letter <= 'z'; letter++) 
+putchar(letter); 
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar ('\n');
 return (0);
 }
