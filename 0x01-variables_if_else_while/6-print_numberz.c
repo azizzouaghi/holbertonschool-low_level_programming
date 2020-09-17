c31de9f3e530401513bf06d6688f597fece9a5cd
azizzouaghi <2181@holbertonschool.com>
2020-09-17 23:55:34 +0100

    if else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+* main - prints out all numbers under 10
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int number;
+for (number = 48; number < 58; number++)
+putchar(number);
+putchar('\n');
+return (0);
+}
