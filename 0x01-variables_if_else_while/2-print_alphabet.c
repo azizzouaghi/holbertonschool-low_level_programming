cc3cb498db138369945aa53c591273f6ccd2d9e9
azizzouaghi <2181@holbertonschool.com>
2020-09-17 19:23:25 +0100

    print alhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * description: random and last digit
+ * Return: Always 0
+ */
+int main(void)
+#include <stdio.h>
+/**
+ * main - main function
+ * description: random and last digit
+ * Return: Always 0
+ */
+int main(void)
+{
+char letter ;
+for( letter = 'a'; letter <= 'z'; letter++)
+{
+putchar ('letter'); 
+}
+  return (0);
+}
+  
