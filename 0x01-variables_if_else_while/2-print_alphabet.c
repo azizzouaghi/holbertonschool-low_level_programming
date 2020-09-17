00b6feefbbf1d202d0567fa3d4cb04521b64a475
azizzouaghi <2181@holbertonschool.com>
2020-09-17 19:55:13 +0100

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 /**
- * main - main function
- * description: random and last digit
- * Return: Always 0
- */
-int main(void)
-#include <stdio.h>
-/**
- * main - main function
- * description: random and last digit
- * Return: Always 0
- */
+* main - main function
+* description: random and last digit
+* Return: Always 0
+*/
 int main(void)
 {
-char letter ;
-for( letter = 'a'; letter <= 'z'; letter++)
+int  letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-putchar ('letter'); 
+putchar (letter);
 }
-  return (0);
+putchar('\n');
+return (0);
 }
-  
