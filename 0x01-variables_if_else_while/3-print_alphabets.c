b75f1852c205164c9cc22896236d56035814a428
azizzouaghi <2181@holbertonschool.com>
2020-09-17 20:24:55 +0100

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - main function
+* description: random and last digit
+* Return: Always 0
+*/
+int main(void)
+{
+int  letter;
+for (letter = 'A'; letter <= 'z'; letter++)
+{
+putchar (letter);
+}
+putchar('\n');
+return (0);
+}
