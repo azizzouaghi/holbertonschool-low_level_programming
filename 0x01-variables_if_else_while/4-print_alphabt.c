0a2627534e09ce3f80c9c01ed8c877aa42725c48
azizzouaghi <2181@holbertonschool.com>
2020-09-17 23:38:34 +0100

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - main function
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+putchar ('\n');
+return (0);
+}
+#include <stdio.h>
+/**
+* main - main function
+* description: random and last digit
+* Return: Always 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+putchar ('\n');
+return (0);
+}
