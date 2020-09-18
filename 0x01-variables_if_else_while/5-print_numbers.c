2413dfa9bd3de0ea695d10729db994976ff57432
azizzouaghi <2181@holbertonschool.com>
2020-09-18 11:01:27 +0100

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
 * main - prints out all numbers under 10
-* 
+*
 * Return: Always 0
 */
 int main(void)
 {
-int number;
-for (number = 0; number < 10; number++)
-printf("%d", number);
-printf("\n");
+int i;
+for (i = 0; i <= 9; i++)
+printf(i);
 return (0);
 }
