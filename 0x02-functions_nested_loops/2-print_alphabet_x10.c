9b843e1554dd83a53c38c3db183c4ef4d8f23414
azizzouaghi <2181@holbertonschool.com>
2020-09-22 00:39:17 +0100

    functions nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+ {
+ for (i=10; i < 10; i++)
+   }   
+while (i< 10)
+}
+ 
+}
