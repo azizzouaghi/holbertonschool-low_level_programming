c5093882cd0d4c23d5e797a87a64ad928f619e16
azizzouaghi <2181@holbertonschool.com>
2020-09-22 11:11:26 +0100

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,18 @@
 */
 void print_alphabet_x10(void)
 {
-int i;
-for (i = 10; i < 10; i++)
-{ char c = 'a';
+  int i, c;
+  c = 'a';
+ for (i = 1; i <= 10; i++)
+{
 while (c <= 'z')
 {
 _putchar(c);
 c++;
 }
+ c = 'a';
 _putchar('\n');
 }
+ return;
 }
 
