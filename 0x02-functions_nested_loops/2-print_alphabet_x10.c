c3d95d249c30dec76e1df3bf4e04b86e90e0d304
azizzouaghi <2181@holbertonschool.com>
2020-09-22 10:59:06 +0100

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+* print_alphabet_x10 - main function
+* description: ptint x10
+* Return: Always 0
+*/
+void print_alphabet_x10(void)
 {
 int i;
- {
- for (i=10; i < 10; i++)
-   }   
-while (i< 10)
-return (0);
+for (i = 10; i < 10; i++)
+{ char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+}
 
-}
- 
-}
