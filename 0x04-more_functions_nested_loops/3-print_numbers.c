9df8a0b342f3cd17344d02852f96da600ee20942
azizzouaghi <2181@holbertonschool.com>
2020-09-24 15:21:03 +0100

    more function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: Always 0
+*/
+void print_numbers(void)
+{
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+puttchar ('\n');
+}
