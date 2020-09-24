6148e7bcab622b31568b0e3defae6967626944d8
azizzouaghi <2181@holbertonschool.com>
2020-09-24 18:14:08 +0100

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_most_numbers - prints numbers 0 to 9 ex 4;2
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' && i != '4')
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
