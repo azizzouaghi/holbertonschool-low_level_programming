eeaeb24f1197418c600c860e9b05bf40150e6c65
azizzouaghi <2181@holbertonschool.com>
2020-09-30 22:39:29 +0100

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*rev_string -reverse string
+*@s: p
+*return: 0
+*/
+void rev_string(char *s)
+{
+int i, j;
+char c;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < (i / 2); j++)
+{
+c = s[j];
+s[j] = s[i - j - 1];
+s[i - j - 1] = c;
+}
+}
+}
