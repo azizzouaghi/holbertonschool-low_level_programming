b1c98b619fcb6900801eeff53f82e6fb9172fc24
azizzouaghi <2181@holbertonschool.com>
2020-10-07 17:54:00 +0100

    puts-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,11 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar(*s);
+if (*s)
+  {
+  _putchar (*s);
 _puts_recursion(s + 1);
-{
-_putchar ('\n');
-}
-}
+ }
+ _putchar ('\n');
+ return ;
 }
