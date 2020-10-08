768f366c363972ecafb30053df8a843744b386b1
azizzouaghi <2181@holbertonschool.com>
2020-10-08 15:03:32 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* *_strstr - locates a substring 
+* @haystack : char
+* @needle : char
+* Return: ;
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i, j = 0;
+for (i = 0; haystack[i]; i++)
+{
+while (haystack[i + j - 1] == needle[j - 1])
+{
+j++;
+if (!needle[j])
+return (&haystack[i]);
+}
+}
+return (0);
+}
