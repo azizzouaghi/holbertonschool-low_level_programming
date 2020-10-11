180a85258e0ed775823f8b870d0629e0cb0cacf3
azizzouaghi <2181@holbertonschool.com>
2020-10-11 16:58:57 +0100

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /**
 * main - entry point
-* 
-*
-*
+*@argc: counts
+*@argv: ;
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
 if (argv)
 {
-printf("%d\n", argc -1);
+printf("%d\n", argc - 1);
 return (0);
 }
 }
