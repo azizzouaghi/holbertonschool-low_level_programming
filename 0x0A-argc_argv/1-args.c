a71ccb289bb7c5e6c2f174c81c62f17917434c94
azizzouaghi <2181@holbertonschool.com>
2020-10-11 18:58:23 +0100

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,8 @@
 * @argv: ;
 * Return: 0
 */
-int main(int argc, char *argv[])
-{
-if (argv)
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
-}
 }
