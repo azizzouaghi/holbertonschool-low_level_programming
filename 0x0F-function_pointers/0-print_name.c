7530c005c89df7a3d972f6d61e2d6600d0b15f9d
azizzouaghi <2181@holbertonschool.com>
2020-10-21 22:12:01 +0100

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*print_name - prints a name
+* print_name - prints a name
 *@name: pointer
-*@f: pionter
+*@f: pointer 
 *Return: ;
 */
 void print_name(char *name, void (*f)(char *))
