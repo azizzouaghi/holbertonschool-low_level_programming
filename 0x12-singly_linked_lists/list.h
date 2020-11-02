a3f616ac61fc032bf9003b84a8555ed9eeda5671
azizzouaghi <2181@holbertonschool.com>
2020-11-02 21:03:08 +0100

    header

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,17 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LIST
+#define LIST
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
 typedef struct list_s
 {
 char *str;
