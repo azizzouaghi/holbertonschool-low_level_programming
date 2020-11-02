604a197359b77ee3c3721e2807c9ff24c4989fc0
azizzouaghi <2181@holbertonschool.com>
2020-11-02 21:30:58 +0100

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*add_node - returns the number of elements in a linked list_t list
+*@head: pointer
+*@str: string
+*Return: ;
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+unsigned int len = 0;
+list_t *tmp;
+tmp = (malloc(sizeof(list_t)));
+if (tmp == NULL)
+{
+return (NULL);
+}
+if ((str == NULL) || (tmp->str != NULL))
+{
+return (NULL);
+free(tmp);
+}
+while (str[len])
+len++;
+tmp->str = strdup(str);
+tmp->len = len;
+tmp->next = *head;
+*head = tmp;
+return (tmp);
+}
