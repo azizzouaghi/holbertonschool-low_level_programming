607634384192e255a8232d5c3ae04f4051125ac7
azizzouaghi <2181@holbertonschool.com>
2020-11-02 20:58:52 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stddef.h>
+/**
+*print_list - prints data of linked list
+*@h: pointer
+*Return: ;
+*/
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+const list_t *tmp;
+tmpp = h;
+if (tmp == NULL)
+{
+return (0);
+}
+while (tmp != NULL)
+{
+if (tmp->str == NULL)
+{
+printf("[%d] (nil)\n", tmp->len);
+tmp = tmp->next;
+i++;
+}
+else
+{
+printf("[%d] %s\n", tmp->len, tmp->str);
+tmpp = tmp->next;
+i++;
+}
+}
+return (i);
+}
