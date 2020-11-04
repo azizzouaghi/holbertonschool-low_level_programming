3eee15ddc261c0bcf67f3e7cfc81f71f600a9b34
azizzouaghi <2181@holbertonschool.com>
2020-11-04 22:24:38 +0100

    print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "lists.h"
 /**
 *print_listint - prints all the elements of a listint_t list
 *@h: pointer
@@ -7,7 +8,7 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i = 0;
-if (h)
+if (h == NULL)
 {
 return (i);
 }
