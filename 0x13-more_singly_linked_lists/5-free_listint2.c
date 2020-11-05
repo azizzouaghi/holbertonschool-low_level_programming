d4b44e7dbf3bb644c2204af7ea8ec94f59d67fe0
azizzouaghi <2181@holbertonschool.com>
2020-11-05 13:58:03 +0100

    free listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*free_listint2 - frees a listint_t list
+*@head: head of list
+*Return: ;
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+tempnode = *head;
+*head = temp->next;
+free(temp);
+}
+*head = NULL;
+}
