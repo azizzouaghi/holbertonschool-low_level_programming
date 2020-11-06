aa1970cdf2f6d1452db1d94911ee25203ae47a66
azizzouaghi <2181@holbertonschool.com>
2020-11-06 20:45:42 +0100

    header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
