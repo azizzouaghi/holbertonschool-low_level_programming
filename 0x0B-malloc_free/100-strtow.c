c37962ec95d71fce09b03af7adce7e229e7ca343
azizzouaghi <2181@holbertonschool.com>
2020-12-04 23:44:15 +0100

    strtow

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* cnt - counts
+* @string: counts
+* Return: ;
+*/
+int cnt(char *string)
+{
+int i, n = 0;
+for (i = 0; string[i]; i++)
+{
+if (string[i] == ' ' && ((string[i + 1] != ' ') && (string[i + 1] != '\0')))
+n++;
+else if (i == 0)
+n++;
+}
+n++;
+return (n);
+}
+/**
+* strtow - splits a string into words
+* @str: string
+* Return: ;
+*/
+char **strtow(char *str)
+{
+int i = 0, j, k, l, n = 0, c = 0;
+char **string;
+if (str == NULL || *str == '\0')
+return (NULL);
+n = cnt(str);
+if (n == 1)
+return (NULL);
+string = malloc(sizeof(char *) * n);
+if (string == NULL)
+return (NULL);
+string[n - 1] = NULL;
+while (str[i])
+{
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+{
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+{
+}
+j++;
+string[c] = malloc(sizeof(char) * j);
+j--;
+if (string[c] == NULL)
+{
+for (k = 0; k < c; k++)
+free(string[k]);
+free(string[n - 1]);
+free(string);
+return (NULL);
+}
+for (l = 0; l < j; l++)
+string[c][l] = str[i + l];
+string[c][l] = '\0';
+c++;
+i = i + j;
+}
+else
+i++;
+}
+return (string);
+}
