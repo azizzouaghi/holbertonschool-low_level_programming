99172536334bd6f45f0466778befbbedcc53ff61
azizzouaghi <2181@holbertonschool.com>
2020-12-14 19:55:39 +0100

    dlistint len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
-* dlistint_len - adds a new node at the beginning of a dlistint_t list
+* dlistint_len - returns the number of elements in a linked dlistint_t list.
 * @h: node
-* Return: the address of the new element, or NULL if it failed
+* Return: ;
 */
 size_t dlistint_len(const dlistint_t *h)
 {
