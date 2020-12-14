f89ae86763e34860fef41352505fe678f9895a27
azizzouaghi <2181@holbertonschool.com>
2020-12-14 21:15:40 +0100

    add dnodeint end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,24 +7,24 @@
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *newElement;
-dlistint_t *store;
-newElement = malloc(sizeof(dlistint_t));
-if (newElement == NULL)
+dlistint_t *end_node;
+dlistint_t *last_node;
+last_node = (*head);
+end_node = malloc(sizeof(dlistint_t));
+if (end_node == NULL)
 return (NULL);
-newElement->n = n;
-newElement->next = NULL;
-store = *head;
+end_node->n = n;
+end_node->next = NULL;
 if (*head == NULL)
 {
-*head = newElement;
-return (newElement);
+end_node->prev = NULL;
+end_node->next = *head;
+*head = end_node;
+return (end_node);
 }
-else
-{
-while (store->next != NULL)
-store = store->next;
-store->next = newElement;
-}
-return (store);
+while (last_node->next != NULL)
+last_node = last_node->next;
+last_node->next = end_node;
+end_node->prev = last_node;
+return (end_node);
 }
